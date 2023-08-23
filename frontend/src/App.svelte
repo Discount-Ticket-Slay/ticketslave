8b0edacf632c92db83457caa1076d570c5bbf291
melly19 <melaniepoon.2022@scis.smu.edu.sg>
2023-08-23 18:02:54 +0800

    Added svelte components

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,19 +3,21 @@
 	import Footer from './components/Footer.svelte';
 	import 'bootstrap/dist/js/bootstrap.js';
 	import Jumbotron from './components/Jumbotron.svelte';
+	import RadioButton from './components/RadioButton.svelte';
+  import LoadingBar from './components/LoadingBar.svelte';
 </script>
 
 <style>
 	@import 'bootstrap/dist/css/bootstrap.css';
-
-	main {
-		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-	}
 </style>
 
 <!-- Rest of app content -->
 <main>
 	<Navbar />
+
+	<RadioButton />
+
+	<LoadingBar />
 
 	<Jumbotron />
 
