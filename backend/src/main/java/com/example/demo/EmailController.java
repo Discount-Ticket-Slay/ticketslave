0ea4d65bbcdeb9664db886c00a46a1d974ea77c7
tengwee12 <tengwee.ong.2022@scis.smu.edu.sg>
2023-09-04 12:45:09 +0800

    modifications to email controller test

--- backend/src/main/java/com/example/demo/EmailController.java
+++ backend/src/main/java/com/example/demo/EmailController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -9,21 +11,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 @RestController
 @RequestMapping("/email")
 public class EmailController {
- 
+
     @Autowired
     private EmailService emailService;
- 
+
     @PostMapping
     public void sendEmail(@RequestBody EmailRequest email) {
-        emailService.sendEmail(email.getTo(), email.getSubject(), email.getBody());
+        // emailService.sendEmail(email.getTo(), email.getSubject(), email.getBody());
+        System.out.println("lets say an email was sent");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<EmailRequest> requestEntity = new HttpEntity<>(emailRequest, headers);
+        String scriptUrl = "URL_OF_YOUR_GOOGLE_SCRIPT_WEB_APP";
+        restTemplate.postForObject(scriptUrl, requestEntity, String.class);
     }
 
     @GetMapping
-    public void doStuff(){
+    public void doStuff() {
         System.out.println("get request sent and received");
     }
 }
