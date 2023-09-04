ee6720496880de6bd5cd444a6db33da153e8f3cd
tengwee12 <tengwee.ong.2022@scis.smu.edu.sg>
2023-09-04 13:37:08 +0800

    got email function to work

--- backend/src/main/java/com/example/demo/EmailController.java
+++ backend/src/main/java/com/example/demo/EmailController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 @RequestMapping("/email")
@@ -21,14 +23,17 @@ public class EmailController {
     @Autowired
     private EmailService emailService;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @PostMapping
     public void sendEmail(@RequestBody EmailRequest email) {
-        // emailService.sendEmail(email.getTo(), email.getSubject(), email.getBody());
+        //emailService.sendEmail(email.getTo(), email.getSubject(), email.getBody());
         System.out.println("lets say an email was sent");
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<EmailRequest> requestEntity = new HttpEntity<>(emailRequest, headers);
-        String scriptUrl = "URL_OF_YOUR_GOOGLE_SCRIPT_WEB_APP";
+        HttpEntity<EmailRequest> requestEntity = new HttpEntity<>(email, headers);
+        String scriptUrl = "https://script.google.com/macros/s/AKfycbwkk-3hTCKma_a2-c4WOKwJ4uLlEw76w2M-6GAI8gU65cZqInQUzVrO7JUFFYBiqDhbWA/exec";
         restTemplate.postForObject(scriptUrl, requestEntity, String.class);
     }
 
