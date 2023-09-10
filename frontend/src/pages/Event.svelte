06c49658dec03ef176288161604e604b17a56ad4
maquilino.2022 <mikhailaquilino@gmail.com>
2023-09-10 16:36:36 +0800

    EventDetails is now a component

--- frontend/src/pages/Event.svelte
+++ frontend/src/pages/Event.svelte
@@ -1,30 +1,12 @@
 <script>
-    import {Button} from 'carbon-components-svelte'
-    import Navbar from '../components/Essentials/Navbar.svelte';
+    import EventDetails from "../components/Events/EventDetails.svelte";
+    import Navbar from "../components/Essentials/Navbar.svelte";
 </script>
 
 <Navbar />
 
-<h5>Taylor Swift | The Eras Tour</h5>
-<h6>March 2, 3, 4 & 7, 8, 9, 2024 | National Stadium</h6><br>
-
-<div class="info-bar">
-    <strong>Event Details</strong>
-    <strong>Ticket Pricing</strong>
-    <strong>Exchange & Refund Policy</strong>
-    <strong>Admission Policy</strong>
-    <Button href="/#/queue">Queue for Tickets</Button>
-</div>
+<EventDetails />
 
 <style>
-    .info-bar {
-        display: flex;
-        justify-content: space-evenly;
-        overflow: hidden;
-    }
-
-    .info-bar > strong {
-        display: table-cell;
-        vertical-align: middle;
-    }
+    
 </style>
