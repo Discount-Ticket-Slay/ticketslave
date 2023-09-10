a1c2533b4bf16b1e22d5cb8a0cc825f41987073a
maquilino.2022 <mikhailaquilino@gmail.com>
2023-09-10 13:14:20 +0800

    can load images into event cards now

--- frontend/src/components/Events/EventCard.svelte
+++ frontend/src/components/Events/EventCard.svelte
@@ -1,27 +1,27 @@
 <script>
-    import {Button} from 'carbon-components-svelte'
-    export let event
+    import { Button } from "carbon-components-svelte";
+    export let event;
 </script>
 
 <div class="card text-center">
-	<img src="" class="card-img-top" alt="No pic found" />
-	<div class="card-body">
-		<h5 class="card-title">{event.eventName}</h5>
+    <img src="public/images/testpic.jpg" class="card-img-top" alt="No pic found" />
+    <div class="card-body">
+        <h5 class="card-title">{event.eventName}</h5>
         <p>{event.eventDescription}</p>
-	</div>
-	<Button href='/#/Event'>Event Details</Button>
+    </div>
+    <Button href="/#/Event">Event Details</Button>
 </div>
 
 <style>
-	@import url('https://fonts.googleapis.com/css2?family=Amaranth&display=swap');
+    @import url("https://fonts.googleapis.com/css2?family=Amaranth&display=swap");
 
-	.card {
-		margin-left: 5%;
-		font-family: 'Amaranth', sans-serif;
-		width: 18rem;
-	}
+    .card {
+        margin-left: 5%;
+        font-family: "Amaranth", sans-serif;
+        width: 18rem;
+    }
 
-	.card-title {
-		width: full;
-	}
+    .card-title {
+        width: full;
+    }
 </style>
