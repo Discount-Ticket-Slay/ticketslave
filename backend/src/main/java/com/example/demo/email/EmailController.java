1b0c1fd8d45780773e09815dff776b8ffefe3a6c
tengwee12 <tengwee.ong.2022@scis.smu.edu.sg>
2023-09-19 22:15:52 +0800

    partial setup of aws rabbitmq

--- backend/src/main/java/com/example/demo/email/EmailController.java
+++ backend/src/main/java/com/example/demo/email/EmailController.java
@@ -2,9 +2,9 @@ package com.example.demo.email;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
