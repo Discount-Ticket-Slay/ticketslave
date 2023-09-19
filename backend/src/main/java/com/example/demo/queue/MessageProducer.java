83a527364ad7d12723f9c9fd87b76a910a2e57f9
tengwee12 <tengwee.ong.2022@scis.smu.edu.sg>
2023-09-20 00:28:02 +0800

    added configuration for ampq, cannot connect for now

---
+++ backend/src/main/java/com/example/demo/queue/MessageProducer.java
@@ -0,0 +1,26 @@
+package com.example.demo.queue;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+@Component
+public class MessageProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+    private final Queue queue; // Inject the Queue bean
+
+    @Autowired
+    public MessageProducer(RabbitTemplate rabbitTemplate, Queue queue) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.queue = queue;
+    }
+
+    public void send(String message) {
+        rabbitTemplate.convertAndSend(queue.getName(), message);
+    }
+}
+
