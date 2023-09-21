38133db8980a28e92af6b5e20db3fc579c3eeddd
maquilino.2022 <mikhailaquilino@gmail.com>
2023-09-22 02:09:38 +0800

    home page improved

--- frontend/src/pages/Home.svelte
+++ frontend/src/pages/Home.svelte
@@ -1,19 +1,104 @@
 <script>
-    import Navbar from '../components/Essentials/Navbar.svelte';
-    import {Button} from 'carbon-components-svelte';
+    import "carbon-components-svelte/css/white.css";
+    const loginButtonStyle = "text-decoration: none;";
 </script>
 
-<style>
-	@import 'bootstrap/dist/css/bootstrap.css';
-</style>
-
-<!-- Rest of app content -->
-<main>
-    <Navbar />
-
-    <div class="container py-2">
-        <Button kind="primary" href="/#/login">Go to: Login</Button>
-        <Button kind = "secondary" href="/#/feed">Go to: Feed</Button>
-        <h1>This is the homepage.</h1>
+<div class="content">
+    <p class="logo">ticketSlave</p>
+    <a href="/#/feed" class="button" style="text-decoration: none;"
+        >Browse Events</a
+    >
+    <div class="login-buttons">
+        <a href="/#/login" class="button login-button" style={loginButtonStyle}
+            >Log In</a
+        >
+        <a href="/#/signup" class="button login-button" style={loginButtonStyle}
+            >Sign up</a
+        >
     </div>
-</main>
+</div>
+
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=GFS+Didot&display=swap");
+
+    .content {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        margin: 0;
+        padding: 0;
+        background-color: beige;
+    }
+
+    .logo {
+        font-family: "GFS Didot", serif;
+        font-size: 6rem;
+
+        animation: fadeIn ease 3s;
+        animation-iteration-count: 1;
+        animation-fill-mode: forwards;
+    }
+
+    @keyframes fadeIn {
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+
+    .login-buttons {
+        margin-top: 2vh;
+    }
+
+    .button {
+        display: inline-block;
+        padding: 0.75rem 1.25rem;
+        border-radius: 10rem;
+        color: #555;
+        text-transform: uppercase;
+        font-size: 1rem;
+        letter-spacing: 0.15rem;
+        transition: all 0.3s;
+        position: relative;
+        overflow: hidden;
+        z-index: 1;
+        cursor: pointer;
+    }
+
+    .button::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: #d2b48c;
+        border-radius: 10rem;
+        z-index: -2;
+    }
+
+    .button::before {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 0%;
+        height: 100%;
+        background-color: #b6916b;
+        transition: all 0.3s;
+        border-radius: 10rem;
+        z-index: -1;
+    }
+
+    .button:hover {
+        color: #fff;
+    }
+
+    .button:hover::before {
+        width: 100%;
+    }
+</style>
