41dba8a0fcc3cdccf821a5ec9cbd4262090d7d83
melly19 <melaniepoon.2022@scis.smu.edu.sg>
2023-10-02 16:02:03 +0800

    Added new-frontend

---
+++ demoapp/frontend2/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Disclosure from '../components/Disclosure.svelte';
+	import Listbox from '../components/Listbox.svelte';
+import Modal from '../components/Modal.svelte';
+</script>
+
+<Modal />
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
