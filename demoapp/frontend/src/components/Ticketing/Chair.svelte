026a35aa0ea94197b6e50bc145d8ac1055caf11f
Ryanljk <ryanlimjk@gmail.com>
2023-10-02 14:32:58 +0800

    debugging

--- demoapp/frontend/src/components/Ticketing/Chair.svelte
+++ demoapp/frontend/src/components/Ticketing/Chair.svelte
@@ -30,12 +30,12 @@
         reservationStatus = !reservationStatus;
         if (reservationStatus) {
 console.log("post pre")
-            updateDatabase("POST", `https://ticketslave.org/tickets/${ticket.ticketId}/reserve`);
+            updateDatabase("POST", `http://www.ticketslave.org/tickets/${ticket.ticketId}/reserve`);
 console.log("post post")
             purchasedTicketsArray.push(ticket);
         }
         else{
-            updateDatabase("PUT", `https://ticketslave.org/tickets/${ticket.ticketId}/undoReserve`);
+            updateDatabase("PUT", `http://www.ticketslave.org/tickets/${ticket.ticketId}/undoReserve`);
             purchasedTicketsArray.filter(item => item !== ticket);
         }
     };
