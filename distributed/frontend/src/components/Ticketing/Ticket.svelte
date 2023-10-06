8871452506dfcda3c153f0d0a26b91d2f2ebcb39
Yue Zhen <smithquaz@gmail.com>
2023-10-06 12:43:33 +0800

    started on microservice

---
+++ distributed/frontend/src/components/Ticketing/Ticket.svelte
@@ -0,0 +1,72 @@
+<!--
+    Individual ticket and ticket details component
+
+    This component is used to display the details of an individual ticket
+    Deatils included: 
+    Seat number (char + 2 digit string)
+    Ticket number
+
+    Features to be added/updated:
+    Additional styling
+-->
+
+<script>
+    import "bootstrap/dist/css/bootstrap.min.css";
+    export let ticket;
+</script>
+
+{#if ticket}
+    <div class="container">
+        <div class="details">
+            <div class="ticket">{`${ticket.rowChar}${ticket.seatNo}`}</div>
+            <div class="category-wrapper">
+                Category: <strong class="category">{ticket.ticketCategory}</strong>
+            </div>
+        </div>
+    </div>
+{:else}
+    <div class="details">
+        Cannot find ticket
+    </div>
+{/if}
+
+<style>
+    .container {
+        display: flex;
+        background-color: #e1e1e1;
+        margin: 0.35vw;
+        padding: 1em;
+        width: 50%;
+        border-radius: 0.5rem;
+    }
+
+    .details {
+        display: flex;
+        flex-direction: column;
+        row-gap: 0.5em;
+        padding-bottom: 1vw;
+    }
+
+    /* .title {
+        margin-top: 0.5em;
+        margin-bottom: 0.5em;
+        color: #8f8f8f;
+    } */
+
+    .ticket {
+        font-size: 1.5em;
+        font-weight: bold;
+    }
+
+    /* .section {
+        font-weight: bolder;
+    } */
+
+    .category-wrapper {
+        margin-bottom: 0.5em;
+    }
+
+    .category {
+        font-weight: bolder;
+    }
+</style>
---
+++ distributed/frontend/src/pages/Login.svelte
@@ -0,0 +1,31 @@
+<script>
+    import LoginInput from '../components/Login/LoginInput.svelte';
+</script>
+
+<style>
+	@import 'bootstrap/dist/css/bootstrap.css';
+
+    .container {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        padding: 10px;
+    }
+</style>
+
+<!-- Rest of app content -->
+<main>
+    <div class="container py-2">
+        <h1>Log In</h1>
+        <br><br><br><br>
+        <LoginInput />
+
+        <br>
+        <p>Don't have an account?</p>
+        <a href='/#/signup'>Sign up</a>
+
+        <br><br>
+        <a href='/#/signup'>Forgot password?</a>
+    </div>
+</main>
