fd9fbee522c9184d0d3c078adfe519782ea6a4a7
innumii <linus.phong.2022@scis.smu.edu.sg>
2023-10-16 12:27:16 +0800

    'testing' basic stuff

--- demoapp/backend/src/test/java/com/example/demo/TicketCategoryTest.java
+++ demoapp/backend/src/test/java/com/example/demo/TicketCategoryTest.java
@@ -6,8 +6,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -17,46 +15,44 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import com.ticketslave.backend.event.Event;
 import com.ticketslave.backend.event.EventRepository;
 import com.ticketslave.backend.event.EventService;
+import com.ticketslave.backend.ticketcategory.TicketCategory;
+import com.ticketslave.backend.ticketcategory.TicketCategoryRepository;
+import com.ticketslave.backend.ticketcategory.TicketCategoryService;
 
 @ExtendWith(MockitoExtension.class)
-public class EventTest {
+public class TicketCategoryTest {
 
     @Mock
     private EventRepository eRepo;
 
     @InjectMocks
     private EventService eServ;
+
+    @Mock
+    private TicketCategoryRepository cRepo;
+
+    @InjectMocks
+    private TicketCategoryService cServ;
     
     @Test
-    void addEvent_ReturnSavedEvent(){
+    void Add_Cat(){
         // arrange ***
         Event event = new Event("last supper", "Betrayal", "01-01-0011", "Long ass table", "Big J", 14);
+        TicketCategory tCat = new TicketCategory("Testing", 10);
 
         // mock the "save" operation 
         when(eRepo.save(any(Event.class))).thenReturn(event);
+        when(cRepo.save(any(TicketCategory.class))).thenReturn(tCat);
 
         // act ***
         Event savedevent = eServ.createEvent(event);
+        TicketCategory savedCat = cServ.createTicketCategory(tCat, event);
         
         // assert ***
         assertNotNull(savedevent);
+        assertNotNull(savedCat);
+        verify(cRepo).save(tCat);
         verify(eRepo).save(event);
     }
 
-    @Test
-    void addEvent_ReturnNoEvent(){
-        // arrange ***
-        Event event = new Event("last supper", "Betrayal", "01-01-0011", "Long ass table", "Big J", -1);
-
-        // mock the "save" operation 
-        when(eRepo.save(any(Event.class))).thenReturn(event);
-
-        // act ***
-        Event savedevent = eServ.createEvent(event);
-        
-        // assert ***
-        assertNotNull(savedevent);
-        verify(eRepo).save(event);
-    }
 }
-
