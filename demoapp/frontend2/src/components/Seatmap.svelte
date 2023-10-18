eadeaa7a7d9affbd63ca5377e49fd336b81ccee7
melly19 <melaniepoon.2022@scis.smu.edu.sg>
2023-10-18 12:21:01 +0800

    Added seat/seatmap components

---
+++ demoapp/frontend2/src/components/Seatmap.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Seat from "./others/Seat.svelte"
+</script>
+
+<Seat />
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
+
