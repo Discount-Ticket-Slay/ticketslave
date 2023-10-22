e2d044e712d5f215d17d83b8941efd198a90898e
melly19 <melaniepoon.2022@scis.smu.edu.sg>
2023-10-22 16:54:12 +0800

    added seatmap

--- distributed/purchase/frontend/src/components/Ticketing/Seatmap.svelte
+++ distributed/purchase/frontend/src/components/Ticketing/Seatmap.svelte
@@ -2,30 +2,31 @@
 <html lang="en">
 	<head>
 		<title>TicketSlave</title>
+		<meta charset="utf-8" />
 
-		<link
-			rel="stylesheet"
-			href="https://cdn.jsdelivr.net/npm/seatchart@0.1.0/dist/seatchart.min.css"
-		/>
-
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/seatchart@0.1.0/dist/seatchart.css" />
 		<style>
+			body {
+				display: flex;
+				justify-content: center;
+			}
+
 			.standing {
 				color: white;
 				background-color: #43aa8b;
 			}
 
-            .cat-1 {
-                color: white;
-                background-color: #277da1;
-            }
+			.cat-1 {
+				color: white;
+				background-color: #277da1;
+			}
 
-            .cat-2 {
-                color: white;
-                background-color: #f8961e;
-            }
+			.cat-2 {
+				color: white;
+				background-color: #f8961e;
+			}
 		</style>
 	</head>
-
 	<body>
 		<div id="container" />
 
@@ -33,23 +34,53 @@
 			type="text/javascript"
 			src="https://cdn.jsdelivr.net/npm/seatchart@0.1.0/dist/seatchart.min.js"
 		></script>
+
 		<script>
-			var element = document.getElementById('container');
 			var options = {
 				map: {
-					rows: 7,
-					columns: 7,
+					rows: 10,
+					columns: 10,
 					seatTypes: {
 						default: {
 							label: 'Standing Pen',
 							cssClass: 'standing',
-							price: 10
+							price: 70
+						},
+						first: {
+							label: 'Cat 1',
+							cssClass: 'cat-1',
+							price: 75,
+							seatRows: [0, 1, 2]
+						},
+						reduced: {
+							label: 'Cat 2',
+							cssClass: 'cat-2',
+							price: 80,
+							seatRows: [7, 8, 9]
 						}
-					}
+					},
+					disabledSeats: [
+						{ row: 0, col: 0 },
+						{ row: 0, col: 9 }
+					],
+					reservedSeats: [
+						{ row: 0, col: 3 },
+						{ row: 0, col: 4 }
+					],
+					selectedSeats: [
+						{ row: 0, col: 5 },
+						{ row: 0, col: 6 }
+					],
+					rowSpacers: [3, 7],
+					columnSpacers: [5]
 				}
 			};
 
+			var element = document.getElementById('container');
 			var sc = new Seatchart(element, options);
+			sc.addEventListener('submit', function handleSubmit(e) {
+				alert('Total: ' + e.total + '$');
+			});
 		</script>
 	</body>
 </html>
