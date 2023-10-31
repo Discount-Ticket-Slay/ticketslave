f8ecf424b8e567456de4bdd8c7d2675b8beb0b8c
innumii <linus.phong.2022@scis.smu.edu.sg>
2023-10-31 23:10:47 +0800

    Added queue Number to the page

--- distributed/queue-service/frontend/src/routes/+page.svelte
+++ distributed/queue-service/frontend/src/routes/+page.svelte
@@ -18,50 +18,62 @@
     //     alert("clicked");
     // };
 
-    let eventId = null;
+    let QueueNo = 100;
 
-    let event = null
-    async function fetchEvent() {
-        try {
-        const hash = window.location.hash;
-        const paramsStart = hash.indexOf('?');
+    // let event = null
+    // async function fetchQueueNo() {
+    //     try {
+    //     const hash = window.location.hash;
+    //     const paramsStart = hash.indexOf('?');
 
-        if (paramsStart >= 0) {
-            const paramString = hash.slice(paramsStart + 1);
-            const urlParams = new URLSearchParams(paramString);
+    //     if (paramsStart >= 0) {
+    //         const paramString = hash.slice(paramsStart + 1);
+    //         const urlParams = new URLSearchParams(paramString);
 
-            let receivedId = urlParams.get("id");
-            console.log(receivedId);
+    //         let receivedId = urlParams.get("id");
+    //         console.log(receivedId);
 
-            if (receivedId) {
-                eventId = Number(receivedId);
-            }
-        }
-    } catch {
-            console.error(error)
-        }
-    }
-    onMount(fetchEvent)
+    //         if (receivedId) {
+    //             QueueNo = Number(receivedId);
+    //         }
+    //     }
+    // } catch {
+    //         QueueNo = -1;
+    //     }
+    // }
+    // onMount(fetchQueueNo)
+
+
+    let fontSize = 55; // initial font size
 </script>
 
 <Navbar />
-
 <div class="min-h-screen flex flex-col">
-    <!-- <Button href="/#/buy-ticket?id={eventId}">Go to: Ticketing</Button> -->
     <div class="centered-div">
+        <p class="queue-text" style="font-size: {fontSize + 'px'}">Your Queue Number: {QueueNo}</p>
+
         <DinoGame />
     </div>
+    
+    <!-- <Button href="/#/buy-ticket?id={eventId}">Go to: Ticketing</Button> -->
+    <!-- <div class="centered-div">
+    </div> -->
 </div>
 
-<Footer />
+<Footer/>
 
 <style>
-    @import "bootstrap/dist/css/bootstrap.css";
+
+    .queue-text {
+            margin-bottom: 100px; /* Adjust this value to control the separation */
+        }
 
     .centered-div {
         position: absolute;
+        top: 50%; /* Adjust this value to control the vertical position */
         left: 50%;
-        transform: translate(-50%);
+        transform: translate(-50%, -50%); /* Center both horizontally and vertically */
         padding: 10px;
+        text-align: center;
     }
 </style>
