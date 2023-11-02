61b3f471417357fb1c06bd6da2285f6f66c366a7
Yue Zhen <smithquaz@gmail.com>
2023-11-03 03:12:04 +0800

    further configuration for cognito

--- distributed/deployment/cloudfront/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "api_gateway_domain_name" {
-    description = "The domain name of the API Gateway"
-    type        = string
-}
--- distributed/deployment/network/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "local_cidr_block" {
-  description = "CIDR block of the local network"
-  type        = string
-}
