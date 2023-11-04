b9625c08a488d24c3f1cda64a9dc91ac4349a5f9
maquilino.2022 <mikhailaquilino@gmail.com>
2023-11-04 21:15:54 +0800

    new feed is like 75% done

--- distributed/feed-service/feed-frontend/src/components/Events/EventCard.svelte
+++ distributed/feed-service/feed-frontend/src/components/Events/EventCard.svelte
@@ -1,40 +1,39 @@
+<!--
+    TODO:
+    an EventCard should display the event name, event date, and event location.
+    the EventCard has a button that opens EventDetails as an overlay.
+-->
+
+<!--EventCard.svelte-->
+
 <script>
-    import "bootstrap/dist/css/bootstrap.min.css";
-    export let event;
-console.log(event.eventId)
+	import EventDetails from "./EventDetails.svelte";
+
+    export let event
+
+    //checks whether any EventCard is clicked on by the user.
+    //if it was clicked, open the EventDetails as an overlay
+    let isEventClicked = false
+
+    function showDetails() {
+        isEventClicked = true
+    }
+
+    function closeDetails() {
+        isEventClicked = false
+    }
+
 </script>
 
-<div class="card text-center">
-    <a href="/#/Event?id={event.eventId}" style="text-decoration: none;">
-        <img
-            src="public/images/concert.jpg"
-            class="card-img-top"
-            alt="No pic found"
-            style="max-width: 100%;"
-        />
-        <div class="card-body">
-            <h5 class="card-title">{event.eventName}</h5>
-        </div>
-    </a>
-</div>
 
-<style>
-    .card {
-        font-family: Verdana, Geneva, Tahoma, sans-serif;
-        width: 12rem;
-        height: auto;
-        cursor: pointer;
-        display: inline-block;
-        margin: 5vh;
-    }
+<button class="p-4 rounded-lg border border-gray-300 shadow-lg h-32 md:h-40 lg:h-48 xl:h-64 cursor-pointer" on:click={showDetails}>
+    <h2 class="text-xl font-semibold">{event.eventName}</h2>
+    <p class="text-sm text-gray-600">{event.eventDate}</p>
+    <p class="text-sm text-gray-600">{event.eventLocation}</p>
+</button>
 
-    .card-body {
-        font-family: Verdana, Geneva, Tahoma, sans-serif;
-        width: full;
-        height: auto;
-    }
-
-    .card-title {
-        width: full;
-    }
-</style>
+{#if isEventClicked}
+    <div>
+        <EventDetails {event} on:close={closeDetails}/>
+    </div>
+{/if}
--- distributed/feed-service/feed-frontend/src/routes/Feed/+page.svelte
+++ distributed/feed-service/feed-frontend/src/routes/Feed/+page.svelte
@@ -1,49 +1,114 @@
+<!--
+    TODO:
+    when the EventCard is clicked, an overlay should open showing EventDetails
+    clicking on a Buy Tickets button in EventDetails should route the user to the buffer service
+-->
+
 <script>
-    import Navbar from "../../components/Essentials/Navbar.svelte"
-    import Footer from "../../components/Essentials/Footer.svelte"
-    import Carousel from "../../components/Others/Carousel.svelte"
-    import Searchbar from "../../components/Others/Searchbar.svelte"
-    import Card from "../../components/Others/Card.svelte"
-    import FaqCard from "../../components/Others/FaqCard.svelte"
-    import EventCard from "../../components/Events/EventCard.svelte"
+	import EventCard from '../../components/Events/EventCard.svelte';
+
+	/**
+     * array of dummy event values.
+     * *will be replaced by actual events in the HTTP GET request
+     */
+	const events = [
+		{
+			eventName: 'Event 1',
+			eventDate: 'Date: November 15, 2023',
+			eventLocation: 'Location: Venue A',
+			eventDescription: 'Join us for an exciting evening filled with music and entertainment.'
+		},
+		{
+			eventName: 'Event 2',
+			eventDate: 'Date: December 5, 2023',
+			eventLocation: 'Location: Venue B',
+			eventDescription: 'A holiday celebration with festive decorations and delicious treats.'
+		},
+		{
+			eventName: 'Event 3',
+			eventDate: 'Date: January 20, 2024',
+			eventLocation: 'Location: Venue C',
+			eventDescription: 'An educational conference featuring industry experts and workshops.'
+		},
+		{
+			eventName: 'Event 4',
+			eventDate: 'Date: February 10, 2024',
+			eventLocation: 'Location: Venue D',
+			eventDescription: 'A fundraising gala to support a local charity and make a difference.'
+		},
+		{
+			eventName: 'Event 5',
+			eventDate: 'Date: March 25, 2024',
+			eventLocation: 'Location: Venue E',
+			eventDescription: 'A food festival showcasing a variety of cuisines from around the world.'
+		},
+		{
+			eventName: 'Event 6',
+			eventDate: 'Date: April 15, 2024',
+			eventLocation: 'Location: Venue F',
+			eventDescription: 'A cultural festival celebrating diversity.'
+		},
+		{
+			eventName: 'Event 7',
+			eventDate: 'Date: May 20, 2024',
+			eventLocation: 'Location: Venue G',
+			eventDescription: 'A science and technology conference with leading experts.'
+		},
+		{
+			eventName: 'Event 8',
+			eventDate: 'Date: June 5, 2024',
+			eventLocation: 'Location: Venue H',
+			eventDescription: 'A sports tournament with teams from around the world.'
+		},
+		{
+			eventName: 'Event 9',
+			eventDate: 'Date: July 10, 2024',
+			eventLocation: 'Location: Venue I',
+			eventDescription: "An art exhibition featuring renowned artists' works."
+		},
+		{
+			eventName: 'Event 10',
+			eventDate: 'Date: August 25, 2024',
+			eventLocation: 'Location: Venue J',
+			eventDescription: 'A charity fundraiser to support a humanitarian cause.'
+		},
+		{
+			eventName: 'Event 11',
+			eventDate: 'Date: September 10, 2024',
+			eventLocation: 'Location: Venue K',
+			eventDescription: 'A culinary festival showcasing local cuisine.'
+		},
+		{
+			eventName: 'Event 12',
+			eventDate: 'Date: October 5, 2024',
+			eventLocation: 'Location: Venue L',
+			eventDescription: 'A fashion show with the latest trends and designs.'
+		},
+		{
+			eventName: 'Event 13',
+			eventDate: 'Date: November 20, 2024',
+			eventLocation: 'Location: Venue M',
+			eventDescription: 'A book fair with authors and book signings.'
+		},
+		{
+			eventName: 'Event 14',
+			eventDate: 'Date: December 10, 2024',
+			eventLocation: 'Location: Venue N',
+			eventDescription: 'A holiday market with unique gifts and decorations.'
+		},
+		{
+			eventName: 'Event 15',
+			eventDate: 'Date: January 5, 2025',
+			eventLocation: 'Location: Venue O',
+			eventDescription: 'An environmental awareness campaign and cleanup event.'
+		}
+	];
 </script>
 
-<body>
-    <Navbar />
-    
-
-    <div class="content">
-        <div class="pt-10">
-            <Searchbar />
-        </div>
-
-        <Carousel />
-
-        <!-- <div class="grid grid-cols-2 gap-4 pt-10 pb-10">
-            <div class="col>"><EventCard /></div>
-        </div> -->
-
-        <div class="grid grid-cols-2 gap-4 pt-10 pb-10">
-            <div class="col>"><Card /></div>
-            <div class="col>"><FaqCard /></div>
-        </div>
-        
-    </div>
-
-    <Footer />
-</body>
-
-<style lang="postcss">
-    :global(html) {
-        background-color: theme(colors.gray.100);
-    }
-
-    .content {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        margin: 0;
-        padding: 0;
-    }
-</style>
+<div class="m-10 min-h-screen rounded-lg">
+	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+		{#each events as event}
+			<EventCard {event} />
+		{/each}
+	</div>
+</div>
