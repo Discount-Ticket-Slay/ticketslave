eda5f2634006d0c6ce01e1a44d29213d008b8d2b
maquilino.2022 <mikhailaquilino@gmail.com>
2023-11-05 15:53:41 +0800

    ahahaahah

--- distributed/purchase-service/frontend/tailwind.config.js
+++ distributed/purchase-service/frontend/tailwind.config.js
@@ -3,12 +3,7 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
-      rotate: {
-        '30': '30deg',
-      },
-      perspective: {
-        '500': '500px',
-      },
+
     },
   },
   plugins: [],
