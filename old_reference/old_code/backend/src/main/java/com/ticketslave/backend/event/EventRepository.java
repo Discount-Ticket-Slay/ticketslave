175dc6507ca1bdd477b7df4d4b8e3568c96716ca
Yue Zhen <smithquaz@gmail.com>
2023-11-05 14:32:05 +0800

    preparing for merge

--- old_reference/old_code/backend/src/main/java/com/ticketslave/backend/event/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ticketslave.backend.event;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-    
-
-}
