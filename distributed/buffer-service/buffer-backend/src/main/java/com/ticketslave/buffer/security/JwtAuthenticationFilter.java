33a539b9a876fcb10c8707e40d65b4351f9729a3
Yue Zhen <smithquaz@gmail.com>
2023-11-05 18:29:24 +0800

    completed merge

--- distributed/buffer-service/buffer-backend/src/main/java/com/ticketslave/buffer/security/JwtAuthenticationFilter.java
+++ distributed/buffer-service/buffer-backend/src/main/java/com/ticketslave/buffer/security/JwtAuthenticationFilter.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
